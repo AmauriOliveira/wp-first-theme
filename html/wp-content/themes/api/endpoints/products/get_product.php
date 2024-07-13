3463873c8086314f0c2208fec91c7e88a19a80ed
Amauri Oliveira <amauriibate32@hotmail.com>
2024-07-13 16:36:32 -0300

    Criação do endpoint get alls products

--- html/wp-content/themes/api/endpoints/products/get_product.php
+++ html/wp-content/themes/api/endpoints/products/get_product.php
@@ -1,7 +1,6 @@
 <?php
 
-function api_get_product($request) {
-  $slug = $request['slug'];
+function product_scheme($slug) {
   $post_id = get_product_by_slug($slug);
 
   if ($post_id) {
@@ -17,7 +16,6 @@ function api_get_product($request) {
           'urls' => $value->guid,
         ];
       }
-
     }
 
     $response = [
@@ -31,19 +29,103 @@ function api_get_product($request) {
 
     ];
 
-  }else {
-    $response = new WP_Error('error', 'Not found: Product with slug ' . $slug . ' not found', ['status' => 404]);
+  } else {
+    $response = new WP_Error('error', "Not found: Product with slug $slug not found", ['status' => 404]);
   }
+
+  return $response;
+}
+
+/**
+ * Retrieves a single product based on the provided slug.
+ *
+ * @param array $request An associative array containing the slug of the product.
+ * @return WP_REST_Response The response containing the product details.
+ */
+function api_get_product($request) {
+
+  $response = product_scheme($request['slug']);
 
   return rest_ensure_response($response);
 }
 
-function register_api_get_product()
-{
+/**
+ * Retrieves a paginated list of published products based on search term and pagination parameters.
+ *
+ * @param array $request An associative array containing the following keys:
+ *                       - 'q' (string): The search term to filter products by. Defaults to an empty string.
+ *                       - 'page' (int): The page number of the results. Defaults to 0.
+ *                       - 'per_page' (int): The number of products per page. Defaults to 10.
+ *                       - 'user_id' (int): The ID of the user making the request. Defaults to an empty string.
+ * @return WP_REST_Response|WP_Error The response containing the paginated list of products and metadata, or a WP_Error object if an error occurs.
+ */
+function api_get_products($request) {
+
+  $search_term = sanitize_text_field($request['q']) ?: '';
+  $page = sanitize_text_field($request['page']) ?: 0;
+  $per_page = sanitize_text_field($request['per_page']) ?: 10;
+  $user_id = sanitize_text_field($request['user_id']);
+
+  $user_id_meta_query = null;
+
+  if ($user_id) {
+    $user_id_meta_query = [
+      'key' => 'post_user_id',
+      'value' => $user_id,
+      'compare' => '='
+    ];
+  }
+
+  // Exclude sold products
+  $not_sold_meta_query = [
+    'key' => 'sold',
+    'value' => 'false',
+    'compare' => '='
+  ];
+
+  $products = new WP_Query([
+    'post_type' => 'product',
+    'post_status' => 'publish',
+    'posts_per_page' => $per_page,
+    'paged' => $page,
+    's' => $search_term,
+    'meta_query' => [
+      $user_id_meta_query,
+      $not_sold_meta_query,
+    ],
+  ]);
+
+  $products_array = [];
+  foreach ($products->posts as $key => $value) {
+    $products_array[$key] = product_scheme($value->post_name);
+  }
+
+  $products_array["current_page"] = (int)$page;
+  $products_array["found_posts"] = $products->found_posts;
+	$products_array["max_num_pages"] = $products->max_num_pages;
+
+  $response = rest_ensure_response($products_array);
+
+  $response->header("X-Page", $page);
+  $response->header("X-Per-Page", $per_page);
+  $response->header("X-Total-Count", $products->found_posts);
+
+  return $response;
+}
+
+function register_api_get_product() {
   register_rest_route('api/v1', 'products/(?P<slug>[-\w]+)', [
     'methods' => WP_REST_Server::READABLE,
     'callback' => 'api_get_product',
   ]);
 }
 
+function register_api_get_products() {
+  register_rest_route('api/v1', 'products', [
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'api_get_products',
+  ]);
+}
+
 add_action('rest_api_init', 'register_api_get_product');
+add_action('rest_api_init', 'register_api_get_products');
